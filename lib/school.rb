25a39c4dbafa12332e85cb76c52ee76da22cd522
Abe D <iaryiou111@gmail.com>
2019-03-13 18:40:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  attr_reader :roster, :name
+  def add_student(name, grade)
+    if self.roster[grade] == nil
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+  end
+  def grade(grade)
+    @roster[grade]
+  end
+  def sort
+    binding.pry
+    @roster.sort_by{|grade, name| [grade, @roster[grade]]}.to_h
+  end
+end
